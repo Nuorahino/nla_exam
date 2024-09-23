d1b5bfdf97e6b29cddddb7a7a0ac9c77f1a58bdb
nuorahino <georg.mix.95s6u@slmail.me>
2024-09-23 18:08:40 +0200

    added householder profiling

--- evaluation/profile/profile.cc
+++ evaluation/profile/profile.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <complex>
 
+#include <eigen3/Eigen/Dense>
+
 #include "../../include/qr.hh"
 #include "../../matrix/matrix_classes.hh"
 #include "../createMatrix.hh"
@@ -16,10 +18,10 @@ int main(int argc, char** argv) {
     size = atoi(argv[1]);
     seed = atoi(argv[2]);
   }
+  Eigen::MatrixXd M = CreateStdRandom<Eigen::MatrixXd>(size, seed);
+  nla_exam::HessenbergTransformation<>(M, true);
 
-  tridiagonal_matrix_nested<double> mat;
-  mat.data.resize(size);
-  CreateStdRandomTri(mat, seed);
+  tridiagonal_matrix_nested<double> mat{M};
 
   auto start = std::chrono::steady_clock::now();
   for (int i = 0; i < 10; ++i) {
--- run.sh
+++ run.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
 cd build/
-variants="-DONE_VEC -DTWO_VEC -DNESTED -DWRAPPED -DARMADILLO -DBLAZE -DLAPACK -DEIGEN"
-#variants="-DONE_VEC -DTWO_VEC -DNESTED -DWRAPPED -DBLAZE -DEIGEN"
+#variants="-DONE_VEC -DTWO_VEC -DNESTED -DWRAPPED -DARMADILLO -DBLAZE -DLAPACK -DEIGEN"
+variants="-DONE_VEC -DTWO_VEC -DNESTED -DWRAPPED -DBLAZE -DEIGEN"
 #variants="-DONE_VEC -DTWO_VEC -DNESTED -DWRAPPED -DARMADILLO"
 compiler="-msse -msse2 -mavx -mavx2 -O3"
 
-cmake ./ -DMY_FLAGS="-DFULL -DREAL_SYMM -DNDEBUG -DUSELAPACK ${variants} ${compiler}"
-#cmake ./ -DMY_FLAGS="-DFULL -DLONGREAL_SYMM -DFLOAT_SYMM -DNDEBUG -DUSELAPACK ${variants} ${compiler}"
+#cmake ./ -DMY_FLAGS="-DFULL -DREAL_SYMM -DNDEBUG ${variants} ${compiler}"
+cmake ./ -DMY_FLAGS="-DFULL -DLONGREAL_SYMM -DNDEBUG ${variants} ${compiler}"
 #cmake ./ -DMY_FLAGS="-DFULL -DLONGREAL_SYMM -DNDEBUG ${variants} ${compiler}"
 #cmake ./ -DMY_FLAGS="-DFULL -DREAL_SYMM -DNDEBUG ${variants} ${compiler}"
 make -B evaluate
-for tol in "1e-8" "1e-11" "1e-14"
+for tol in "1e-16" "1e-11" "1e-14"
+do
+  echo "tol $tol"
+  for seed in {1..10}
+  do
+    echo "seed $seed"
+    echo "compiler options $compiler"
+    echo $(date +%T)
+    ./evaluation/evaluate 1 1000 $seed $tol
+  done
+done
+
+variants="-DONE_VEC -DTWO_VEC -DNESTED -DWRAPPED -DARMADILLO -DBLAZE -DLAPACK -DEIGEN"
+
+#cmake ./ -DMY_FLAGS="-DFULL -DREAL_SYMM -DNDEBUG ${variants} ${compiler}"
+cmake ./ -DMY_FLAGS="-DFULL -DFLOAT_SYMM -DNDEBUG -DUSELAPACK ${variants} ${compiler}"
+#cmake ./ -DMY_FLAGS="-DFULL -DLONGREAL_SYMM -DNDEBUG ${variants} ${compiler}"
+#cmake ./ -DMY_FLAGS="-DFULL -DREAL_SYMM -DNDEBUG ${variants} ${compiler}"
+make -B evaluate
+for tol in "1e-8" "1e-11"
 do
   echo "tol $tol"
   for seed in {1..10}
